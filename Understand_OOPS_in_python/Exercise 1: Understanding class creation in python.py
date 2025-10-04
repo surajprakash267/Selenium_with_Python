a84dd3cac43b94f5ca1a280c902c9bfd4f00d495
mac <mac@macs-MacBook-Pro.local>
2025-10-04 15:38:50 +0530

    Initial commit

---
+++ Understand_OOPS_in_python/Exercise 1: Understanding class creation in python.py
@@ -0,0 +1,30 @@
+"""
+Understand how to manipulate strings and print them.
+Create a variable named greeting and assign it the string "Welcome to Python Programming".
+Print the greeting variable.
+Modify the string to include your instructor name "Rahul!". For example, "Welcome to Python Programming, [Instructor Name]!" and print the modified string.
+Expected Result:
+
+Welcome to Python Programming
+Welcome to Python Programming, Rahul!
+
+"""
+#ANSWER
+
+
+#greeting = "Welcome to Python Programming"
+#print(greeting)
+#greeting_with_name = "Welcome to Python Programming,Rahul!"
+#print(greeting_with_name)
+
+greeting = "Welcome to Python Programming"
+
+print(greeting)
+
+
+
+# Modify the string to include your instructor name "Rahul!"
+
+greeting_with_name = "Welcome to Python Programming, Rahul!"
+
+print(greeting_with_name)
