ddba26031d62d02173c9a2a96da71c74e8f9000f
surajprakash267 <surajprakash267@gmail.com>
2025-10-04 15:49:02 +0530

    2

---
+++ Understand how to manipulate strings and print them/Variable Assignment and Types.py
@@ -0,0 +1,25 @@
+"""
+Create three variables: age, height, and favorite_color. Assign them values 25, 5.9, blue:
+age: an integer (e.g., 25)
+height: a float (e.g., 5.9)
+favorite_color: a string (e.g., "blue")
+Use the print function to display each variable and its type using the type() function.
+Expected Result:
+
+Age: 25 | Type: <class 'int'>
+Height: 5.9 | Type: <class 'float'>
+Favorite Color: blue | Type: <class 'str'>
+"""
+
+
+# ANSWERS #
+
+age = 25
+height = 5.9
+favorite_color = "blue"
+
+print("Age:", age, "| Type:", type(age))
+
+print("Height:", height, "| Type:", type(height))
+
+print("Favorite Color:", favorite_color, "| Type:", type(favorite_color))
