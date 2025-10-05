35da90829169a436a65c38dd87caa05b69577d5b
surajprakash267 <surajprakash267@gmail.com>
2025-10-05 22:57:36 +0530

    Doubling Numbers with For Loop

---
+++ Program Flow in Python/Exercise 8: Doubling Numbers with For Loop.py
@@ -0,0 +1,19 @@
+"""
+1. Create a list of numbers: [1, 4, 7, 10].
+2. Use a for loop to iterate through the list.
+3. Inside the loop, print each number multiplied by 3.
+
+Expected Output:
+3
+12
+21
+30
+
+"""
+
+#                               ANSWER
+
+numbers = [1, 4, 7, 10]
+
+for i in numbers:
+    print(i*3)
