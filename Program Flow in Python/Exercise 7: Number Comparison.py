2bc4bc8741222a596f492e237dca804567ea9df7
surajprakash267 <surajprakash267@gmail.com>
2025-10-05 22:55:49 +0530

    Number comparison in python

---
+++ Program Flow in Python/Exercise 7: Number Comparison.py
@@ -0,0 +1,26 @@
+"""
+Create a variable b and assign it a number 15.
+Write an if statement to check if b is greater than 10.
+If true, print "Number is greater than 10".
+If false, print "Number is 10 or less".
+Print "Comparison code is completed" at the end.
+Expected Output:
+For b = 15:
+Number is greater than 10
+Comparison code is completed.
+For b = 5:
+Number is 10 or less
+Comparison code is completed.
+
+"""
+
+#                                   ANSWER
+
+b = 15
+
+if b > 10:
+    print("Number is greater than 10")
+else:
+    print("Number is 10 or less")
+
+print("Comparison code is completed.")
