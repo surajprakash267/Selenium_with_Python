b4cf3e030143084845cd5b720a4af3ca5b0fdeb4
surajprakash267 <surajprakash267@gmail.com>
2025-10-05 22:48:28 +0530

    Exploring Tuples

---
+++ Understanding Python datatypes and variables/Exercise 4: Exploring Tuples.py
@@ -0,0 +1,14 @@
+"""
+Create a tuple named person that contains three elements: a name (string), an age (integer), and a height (float) with the below values.
+"Rahul", 25, 5.9
+Print the second element of the tuple.
+
+Expected Result:
+
+Age: 25
+"""
+
+#                                   ANSWER
+
+person = ("Rahul", 25, 5.9)
+print("Age:", person[1])
