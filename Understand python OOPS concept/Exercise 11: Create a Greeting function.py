563ecad179466290af89061316351288d107a98f
surajprakash267 <surajprakash267@gmail.com>
2025-10-06 23:54:11 +0530

    Creating a Greeting function

---
+++ Understand python OOPS concept/Exercise 11: Create a Greeting function.py
@@ -0,0 +1,18 @@
+"""
+Objective: Create a function that greets the user.
+Instructions:
+Write a function called GreetUser that takes a single argument username.
+The function should print "Hello, [username]! Welcome to the Python course."
+Call the function with username "John".
+Expected Output:
+Hello, John! Welcome to the Python course.
+
+"""
+
+#                                   ANSWER
+
+def GreetUser(username):
+    print(f"Hello, {username}! Welcome to the Python course.")
+
+
+GreetUser("John")
